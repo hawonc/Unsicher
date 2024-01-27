0667a042f56a6ae9b87ca03c8679c19459ff375e
hawonc <hawonc@umich.edu>
2024-01-27 15:16:08 -0500

    basic ip logger

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const requestIp = require('request-ip')
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+var Fingerprint = require('express-fingerprint')
+
+const parseIp = (req) =>
+    req.headers['x-forwarded-for']?.split(',').shift()
+    || req.socket?.remoteAddress
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
+
+app.use(Fingerprint({
+  parameters:[
+      Fingerprint.useragent,
+      Fingerprint.acceptHeaders,
+      Fingerprint.geoip,
+  ]
+}))
+
+app.get('*',function(req,res,next) {
+  // Fingerprint object
+  console.log(JSON.stringify(req.fingerprint))
+  console.log(requestIp.getClientIp(req))
+  console.log(parseIp(req))
+  res.send("Hi, hope you enjoy sharing")
+})
